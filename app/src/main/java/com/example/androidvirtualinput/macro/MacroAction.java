4c8b22d6047fa7e9f0bd2fb8e639611d04550b4d
road-pig <qiuzeeming@gmail.com>
2021-04-11 20:14:22 +0800

    made macros simpler so windows api supports it

--- app/src/main/java/com/example/androidvirtualinput/macro/MacroAction.java
+++ app/src/main/java/com/example/androidvirtualinput/macro/MacroAction.java
@@ -8,48 +8,34 @@ import java.util.ArrayList;
 
 public class MacroAction extends InputAction {
     private final ArrayList<Integer> keys;
-    private final ArrayList<Double> durations;
-    private final ArrayList<Double> delays;
-    private final ArrayList<Integer> startModes; //0 -> with previous 1 -> after previous
 
     private final String name;
 
-    public MacroAction(ArrayList<Integer> keys, ArrayList<Double> durations, ArrayList<Double> delays, ArrayList<Integer> startModes, String name) {
+    public MacroAction(ArrayList<Integer> keys, String name) {
         this.keys = keys;
-        this.durations = durations;
-        this.delays = delays;
-        this.startModes = startModes;
         this.name = name;
     }
-    //NAME:KEY|DURATION|DELAY|STARTMODE,KEY|DURATION|DELAY|STARTMODE,...
+    //NAME:KEY1,KEY2,KEY3...
     public MacroAction(String actionString){
         keys = new ArrayList<>();
-        durations = new ArrayList<>();
-        delays = new ArrayList<>();
-        startModes = new ArrayList<>();
 
         String[] splitForName = actionString.split(":");
         name = splitForName[0];
 
         String[] splitForKeys = splitForName[1].split(",");
 
-        String[] splitForEachKey;
         for (String splitForKey : splitForKeys) {
-            splitForEachKey = splitForKey.split("\\|");
-            keys.add(Integer.parseInt(splitForEachKey[0]));
-            durations.add(Double.parseDouble(splitForEachKey[1]));
-            delays.add(Double.parseDouble(splitForEachKey[2]));
-            startModes.add(Integer.parseInt(splitForEachKey[3]));
+            keys.add(Integer.parseInt(splitForKey));
         }
     }
     //for sending over the socket
-    //MACRO:KEY|DURATION|DELAY|STARTMODE,KEY|DURATION|DELAY|STARTMODE,...
+    //MACRO:KEY1,KEY2,KEY3...
     @Override
     public String getActionString() {
         StringBuilder result = new StringBuilder("MACRO:");
 
         for (int i = 0; i < keys.size(); i++){
-            result.append(keys.get(i)).append("|").append(durations.get(i)).append("|").append(delays.get(i)).append("|").append(startModes.get(i));
+            result.append(keys.get(i));
             if(i != keys.size()){
                 result.append(",");
             }
@@ -64,7 +50,7 @@ public class MacroAction extends InputAction {
         StringBuilder result = new StringBuilder(name).append(":");
 
         for (int i = 0; i < keys.size(); i++){
-            result.append(keys.get(i)).append("|").append(durations.get(i)).append("|").append(delays.get(i)).append("|").append(startModes.get(i));
+            result.append(keys.get(i));
             if(i != keys.size()){
                 result.append(",");
             }
